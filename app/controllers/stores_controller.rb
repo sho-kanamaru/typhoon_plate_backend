79d50dc03073f8f179a88dab15adb97b7284cedb
sho <sho02.soccer@gmail.com>
2018-06-09 16:47:56 +0900

    edit

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -6,7 +6,7 @@ class StoresController < ApplicationController
     visit_count_by_user_id = Checkin.joins(:store).where(store_id: store_id).group(:user_id).order(user_id: :desc).limit(5).count()
     @ranking = []
     visit_count_by_user_id.each do |count|
-      r = {user_id: count[0], count: count[1]}
+      r = {user_id: count[0], user_name: User.find(count[0]).name, count: count[1]}
       @ranking.push(r)
     end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     store_visit_counts = user.checkins.group(:store_id).count
     @personal_array = []
     store_visit_counts.each do |count|
-      personal = {"store_id": count[0], "visit_count": count[1]}
+      personal = {"store_id": count[0], "store_name": Store.find(count[0]).name, "visit_count": count[1]}
       @personal_array.push(personal)
     end
     render json: {array: @personal_array, name: @name}
